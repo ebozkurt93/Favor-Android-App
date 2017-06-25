bd236d806443f62f06e3adc9fbaed45c30c9f5c8
Erdem Bozkurt <ebozkurt93@gmail.com>
2017-06-25 17:23:01 +0300

    added library, do changes later

--- app/src/main/java/android/ebozkurt/com/favor/BoonListActivity.java
+++ app/src/main/java/android/ebozkurt/com/favor/BoonListActivity.java
@@ -5,9 +5,12 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem;
+
 public class BoonListActivity extends AppCompatActivity {
 
-    private BottomNavigationView bottomNavigationView;
+    private AHBottomNavigation bottomNavigationView;
 
 
     @Override
@@ -15,9 +18,24 @@ public class BoonListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_boon_list);
 
-        bottomNavigationView = (BottomNavigationView) findViewById(R.id.activity_boon_list_bottom_navigation_bar);
-        BottomNavigationViewHelper.removeShiftMode(bottomNavigationView);
+        bottomNavigationView = (AHBottomNavigation) findViewById(R.id.activity_boon_list_bottom_navigation_bar);
+        //BottomNavigationViewHelper.removeShiftMode(bottomNavigationView);
 
+        AHBottomNavigationItem home = new AHBottomNavigationItem(R.string.home, R.drawable.google_vector, R.color.colorPrimary);
+        AHBottomNavigationItem chat = new AHBottomNavigationItem(R.string.chat, R.drawable.google_vector, R.color.colorPrimary);
+        AHBottomNavigationItem create = new AHBottomNavigationItem(R.string.create, R.drawable.google_vector, R.color.colorPrimary);
+        AHBottomNavigationItem profile = new AHBottomNavigationItem(R.string.profile, R.drawable.google_vector, R.color.colorPrimary);
+        AHBottomNavigationItem more = new AHBottomNavigationItem(R.string.more, R.drawable.google_vector, R.color.colorPrimary);
 
+        bottomNavigationView.addItem(home);
+        bottomNavigationView.addItem(chat);
+        bottomNavigationView.addItem(create);
+        bottomNavigationView.addItem(profile);
+        bottomNavigationView.addItem(more);
+        bottomNavigationView.setTitleState(AHBottomNavigation.TitleState.ALWAYS_HIDE);
+      //  bottomNavigationView.setColored(true);
+//        bottomNavigationView.setForceTint(true);
+        bottomNavigationView.setCurrentItem(0);
+        bottomNavigationView.setNotification("Berkin",3);
     }
 }
